513eafa15b351b303de6c83b74e0181d7e4a9d1c
Bozska_XD <85171769+BozskaTheLOL@users.noreply.github.com>
2024-09-18 14:45:57 +0200

    Navigation files
    New files

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+document.getElementById("openmenu").onclick() 
+{
+    document.getElementById("navigáció").classList.toggle("nyitva");
+}
+*/
+
+function openMenu(){
+    document.getElementById("nav").classList.toggle("open")
+}
